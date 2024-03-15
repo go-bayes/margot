ca5a668bf27882e25173dcce2a8abc44e605b42f
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2024-03-15 21:16:08 +1300

    update

--- R/margot_wide_impute_baseline.R
+++ R/margot_wide_impute_baseline.R
@@ -33,25 +33,102 @@ margot_wide_impute_baseline <- function(.data, baseline_vars, exposure_var, outc
   if (!is.data.frame(.data)) {
     stop("The provided data is not a data frame.")
   }
-  # Use margot_wide to transform data into wide format
-  wide_data <- margot_wide(.data, baseline_vars, exposure_var, outcome_vars)
 
-  # Identify columns for baseline (t0_) that need imputation
-  t0_columns <- names(wide_data)[grepl("^t0_", names(wide_data)) &
-                                   names(wide_data) %in% paste0("t0_", c(baseline_vars, exposure_var, outcome_vars))]
+  # add a check for unused levels of factor variables
+  lapply(.data, function(column) {
+    if (is.factor(column) && any(table(column) == 0)) {
+      stop("There are unused levels in the factor variable: ",
+           deparse(substitute(column)))
+    }
+  })
 
-  if (length(t0_columns) > 0) {
-    # Impute missing values for baseline variables
-    t0_data <- wide_data[t0_columns]
-    imputed_data <- mice::mice(t0_data, method = 'pmm', m = 1)
-    complete_t0_data <- mice::complete(imputed_data, 1)
+  # add the 'time' column to the data
+  data_with_time <- .data |>
+    mutate(time = as.numeric(wave) - 1) |>
+    arrange(id, time)
 
-    # Merge the imputed data back into the wide data
-    wide_data[, t0_columns] <- complete_t0_data
+  # Filter the data based on the time condition
+  data_filtered <- data_with_time |>
+    filter(time >= 0)
+
+  # Create the wide data frame
+  wide_data <- data_filtered |>
+    pivot_wider(
+      id_cols = id,
+      names_from = time,
+      values_from = -c(id, time),
+      names_glue = "t{time}_{.value}",
+      names_prefix = "t"
+    )
+
+  # Identify the columns starting with "t0_" that need to be imputed
+  t0_columns <-
+    grepl("^t0_", names(wide_data)) &
+    names(wide_data) %in% paste0("t0_", c(baseline_vars, exposure_var, outcome_vars))
+
+  # Apply the imputation
+  t0_data <- wide_data[, t0_columns, drop = FALSE]
+  imputed_data <- mice(t0_data, method = 'pmm', m = 1)
+  complete_t0_data <- complete(imputed_data, 1)
+
+  # Merge the imputed data back into the wide data
+  wide_data[, t0_columns] <- complete_t0_data
+
+  # Define a custom function to filter columns based on conditions
+  custom_col_filter <- function(col_name) {
+    if (startsWith(col_name, "t0_")) {
+      return(col_name %in% c(
+        paste0("t0_", baseline_vars),
+        paste0("t0_", exposure_var),
+        paste0("t0_", outcome_vars)
+      ))
+    } else if (startsWith(col_name, "t1_")) {
+      return(col_name %in% paste0("t1_", exposure_var))
+    } else if (grepl("^t[2-9][0-9]*_", col_name)) {
+      return(col_name %in% paste0("t2_", outcome_vars))
+    } else {
+      return(FALSE)
+    }
   }
 
-  return(wide_data)
+  # Apply the custom function to select the desired columns
+  wide_data_filtered <- wide_data |>
+    dplyr::select(id, which(sapply(
+      colnames(wide_data), custom_col_filter
+    ))) |>
+    dplyr::relocate(starts_with("t0_"), .before = starts_with("t1_"))  |>
+    arrange(id)
+
+  # Extract unique time values from column names
+  time_values <-
+    gsub("^t([0-9]+)_.+$", "\\1", colnames(wide_data_filtered))
+  time_values <- time_values[grepl("^[0-9]+$", time_values)]
+  time_values <- unique(as.numeric(time_values))
+  time_values <- time_values[order(time_values)]
+
+  # Relocate columns iteratively
+  for (i in 2:(length(time_values) - 1)) {
+    wide_data_filtered <- wide_data_filtered |>
+      dplyr::relocate(starts_with(paste0("t", time_values[i + 1], "_")), .after = starts_with(paste0("t", time_values[i], "_")))
+  }
+
+  # Reorder t0_ columns
+  t0_column_order <-
+    c(
+      paste0("t0_", baseline_vars),
+      paste0("t0_", exposure_var),
+      paste0("t0_", outcome_vars)
+    )
+  wide_data_ordered <- wide_data_filtered |>
+    select(id, t0_column_order, everything())
+
+  return(data.frame(wide_data_ordered)) # Ensure output is a data.frame
+
+  return(data.frame(wide_data_ordered)) # Ensure output is a data.frame
 }
+
+
+
 
 
 
