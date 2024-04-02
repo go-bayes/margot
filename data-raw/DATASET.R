a30136de5bc4b2abfdbdd04a3b4029cf8abfcb3a
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2024-04-02 16:11:39 +1300

    fake data
    fixed male indicator so more females than males

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,25 +1,23 @@
-# code to prepare `DATASET` dataset goes here
-if (!requireNamespace("haven", quietly = TRUE)) {
-  install.packages("haven")
-}
-
-# local path (for jb)
-push_mods <-
-  fs::path_expand(
-    "/Users/joseph/Library/CloudStorage/Dropbox-v-project/Joseph\ Bulbulia/00Bulbulia\ Pubs/DATA/nzavs-current/r-data"
-  )
-
-library(dplyr)
-library(tidyverse)
-# # read arrow
-df_nz <-
-  arrow::read_parquet(here::here(push_mods, "df_nz_synth_data_t14"))
-
-str(df_nz)
-str( df_nz$male)
+# # code to prepare `DATASET` dataset goes here
+# # if (!requireNamespace("haven", quietly = TRUE)) {
+# #   install.packages("haven")
+# # }
+#
+# # local path (for jb)
+# # push_mods <-
+# #   fs::path_expand(
+# #     "/Users/joseph/Library/CloudStorage/Dropbox-v-project/Joseph\ Bulbulia/00Bulbulia\ Pubs/DATA/nzavs-current/r-data"
+# #   )
+#
+# library(dplyr)
+# library(tidyverse)
+# # # read arrow
+# df_nz <-
+#   qs::qread(here::here(push_mods, "df_nz_synth_data_t14"))
+#
 # # check
-colnames(df_nz)
-str(df_nz$male)
+# colnames(df_nz)
+# str(df_nz$male)
 #
 # # save data
-usethis::use_data(df_nz, overwrite = TRUE)
+# usethis::use_data(df_nz, overwrite = TRUE)
