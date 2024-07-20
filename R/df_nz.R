3d747d4c3b1cf0d6b1e53e130c7ca2f8386899e5
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2024-07-21 00:05:47 +1200

    copyright for simulated data

---
+++ R/df_nz.R
@@ -0,0 +1,30 @@
+#' df_nz: Example Data Frame
+#'
+#' This dataset contains simulated data from The New Zealand Attitudes and Values Study.
+#'
+#' @docType data
+#' @name df_nz
+#' @usage data(df_nz)
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{age}{participant age in years}
+#'   \item{agreeableness}{Score on the agreeableness scale. Includes items such as:
+#'     \describe{
+#'       \item{i.}{I sympathize with others' feelings.}
+#'       \item{ii.}{I am not interested in other people's problems. (reversed)}
+#'       \item{iii.}{I feel others' emotions.}
+#'       \item{iv.}{I am not really interested in others. (reversed)}
+#'     }
+#'   }
+#'   \item{...}{Other variables}
+#' }
+#' @source Simulated data
+#'
+#' @details
+#' This dataset is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+#' © 2024, Curators of the Margot Package. See LICENSE-data.md for more details.
+#'
+#' @examples
+#' data(df_nz)
+#' head(df_nz)
+"df_nz"
