4d8e6bfeee34df9c43ffa45c55e725782720b20d
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2024-08-08 01:19:55 +1200

    Qini
      ensure qini_obj has a '_path' and 'gain' attribute

--- R/extract_qini_data.R
+++ R/extract_qini_data.R
@@ -11,11 +11,23 @@
 #'
 #' @keywords internal
 extract_qini_data <- function(qini_obj, arm_name, max_index) {
+  # Ensure qini_obj has a '_path' and 'gain' attribute
+  if (is.null(qini_obj[["_path"]]) || is.null(qini_obj[["_path"]]$gain)) {
+    warning(paste("Qini object is missing '_path' or 'gain' for arm:", arm_name))
+    return(data.frame(index = integer(0), gain = numeric(0), arm = character(0)))
+  }
+
   gain <- qini_obj[["_path"]]$gain
   index <- seq_along(gain)
+
   # extend the gain to the max_index with the last value
   extended_gain <- c(gain, rep(tail(gain, 1), max_index - length(gain)))
   extended_index <- seq_len(max_index)
+
+  # Print statements for debugging
+  cat(paste("Extracting Qini data for:", arm_name, "\n"))
+  cat(paste("Gain length:", length(gain), "\n"))
+  cat(paste("Extended gain length:", length(extended_gain), "\n"))
 
   data.frame(
     index = extended_index,
